718050671647b96a264950e9d3c832808c9a593a
Kylie Lennon <klennon525@gmail.com>
2024-03-22 12:36:58 -0400

    User home page formatting

--- client/src/pages/UserHomePage.js
+++ client/src/pages/UserHomePage.js
@@ -22,16 +22,36 @@ export default function UserHome({}) {
 
     return (
         <div>
-        <h1>User Home Page</h1>
-        <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-1/3'onClick={handleEventButtonClick}>
-            Plan an Event
-        </button>
-        <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-1/3'onClick={handleScheduleButtonClick}>
-            Your Schedule
-        </button>
-        <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-1/3'onClick={handleFriendsButtonClick}>
-            Friends
-        </button>
+            <div className='flex text-center justify-center bg-blue-400 text-5xl pt-8 text-white pb-4'>
+                Welcome to Time2Meet!
+            </div>
+            <div className='flex text-center justify-center	bg-blue-400 text-2xl pb-8 text-white'>
+                In this website you are able to create schedule and share your plans with friends without the extra hassle.
+            </div>
+            <div className='grid grid-cols-2 content-center justify-center place-content-center'>
+                <div className='flex col-start-1 align-center grid grid-rows-4 h-full bg-blue-400 rounded-3xl p-8 m-8'>
+                    <div className='flex justify-center text-white text-4xl'>
+                        Things to Do!
+                    </div>
+                    <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2'onClick={handleEventButtonClick}>
+                        Plan an Event
+                    </button>
+                    <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2'onClick={handleScheduleButtonClick}>
+                        Your Schedule
+                    </button>
+                    <button className='bg-blue-800 text-white border-2 border-blue-500 hover:bg-white hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 font-medium rounded-2xl text-sm px-5 py-2.5 mr-2 mb-2'onClick={handleFriendsButtonClick}>
+                        Friends
+                    </button>
+                </div>
+                <div className='flex col-start-2 align-center grid grid-rows-4 h-full bg-blue-400 rounded-3xl p-8 m-8'>
+                    <div className='flex justify-center text-white text-4xl'>
+                        Upcoming Events
+                    </div>
+                    <div>
+                        Events go here!
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

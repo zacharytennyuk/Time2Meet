002e49a571e42a8c75c1b4573ce742e3d8f54e6e
waugustin <waugustin@ufl.edu>
2024-03-30 15:16:18 -0400

    friend backend

--- client/src/pages/FriendsPage.js
+++ client/src/pages/FriendsPage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CreateAccountTextBox from '../components/createAccountTextBox';
+import SubmitButton from '../components/submitButton';
 import { useNavigate } from 'react-router-dom'
 
 export default function Friends({}) {
     const navigate = useNavigate();
+
+    const [friends, setFriends] = useState({
+        addFriends: '',
+        deleteFriends: ''
+    });
     
+      //  update event state when input fields change
+      const updateFriends = (event) => {
+        const { name, value } = event.target;
+        setFriends(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+    
+    const addFriends = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/api/friends', friends);
+           // fetchFriends();
+
+        } catch (error) {
+            console.error('Error While Creating Event', error);
+        }
+    };
+
     // Event handler for Home button click
     const handleHomeButtonClick = () => {
         navigate('/user-home');
@@ -45,6 +73,28 @@ export default function Friends({}) {
                     </div>
                 </div>
             </div>
+        <h1>Friends Page</h1>
+            <button onClick={handleHomeButtonClick}>
+                {/* SVG Icon for House */}
+                <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                    <polygon points="25 5 45 25 5 25" stroke="rgb(30 58 138)" stroke-width="5"stroke-linecap="round" fill="rgb(30 58 138)" stroke-linejoin="round"/>
+                    <polygon points="10 20 40 20 40 45 10 45 "stroke="rgb(30 58 138)" stroke-width="5"stroke-linecap="round" fill="rgb(30 58 138)" stroke-linejoin="round"/>
+                    <polygon points="38 10 38 20 "stroke="rgb(30 58 138)" stroke-width="7.5"stroke-linecap="round" fill="rgb(30 58 138)" stroke-linejoin="round"/>
+                    <polygon points="20 30 20 48 30 48 30 30"stroke="white" stroke-width="5"stroke-linecap="round" fill="white" stroke-linejoin="round"/>
+                </svg>
+            </button>
+            <h2>Your Friends </h2>
+
+            <form onSubmit = {addFriends}>
+              <CreateAccountTextBox
+                    label="Invite Friends"
+                    type="text"
+                    name= "addFriends"
+                    value={friends.addFriends}
+                    onChange={updateFriends}
+                />
+              <SubmitButton> Add Friend </SubmitButton>
+            </form>
         </div>
     );
 }
